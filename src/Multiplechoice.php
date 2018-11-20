b25b2571c4d1f492f00f519eaf90433cbf7644a5
Giuliano Regolo <gripdesexto6e@gmail.com>
2018-11-20 10:32:28 -0300

    depedazo el archivo yaml

--- src/Multiplechoice.php
+++ src/Multiplechoice.php
@@ -8,11 +8,49 @@ use Symfony\Component\Yaml\Parser;
 class Multiplechoice{
     private $pregun;
     private $cant;
-    private $preguntasExamen;
+    private $descipciones;
+    private $ocultartodasAnteriores;
+    private $ocultarNingunatodasAnteriores;
 
     public function __construct($cant) {   
         $this->cant = $cant;
         $this->pregun = Yaml::parseFile('/Preguntas/preguntas.yml');
+    }
+
+    public function Generarpregunta(){
+        $contador = 0; 
+        foreach($this->pregun as $pregunta){
+            $descipciones[$contador] = $pregunta[descripcion];
+            $this->respuesta_incorrectas[$contador] = $pregunta[respuestas_incorrectas];
+            $this->respuestas_correctas[$contador] = $pregunta[respuestas_correctas];
+            if (array_key_exists('ocultar_opcion_todas_las_anteriores',$preguntas)){
+                $this->ocultartodasAnteriores[$contador] = true; 
+            }
+            else{
+                $this->ocultartodasAnteriores[$contador] = false;
+            }
+            if(array_key_exists('ocultas_opcion_ninguna_de_las_anteriores',$pregunta)){
+                $this->ocultarNingunatodasAnteriores[$contador] = true;
+            }
+            else{
+                $this->ocultarNingunatodasAnteriores[$contador] = false;
+            }
+        $contador++;
+        }
+
+    }
+
+    public function opciones($cant){
+        
+        if ($this->respuesta_incorrectas[$cant] = []){
+            $this->respuest_correcta[$cant+1] ='Todas son correctas'; 
+            
+        }
+        if ($this->respuesta_correctas[$cant] = []){
+            $this->respuest_correcta[$cant+1] ='Ninguna es correcta'; 
+        }
+
+
     }
 
     public function CrearTema($pregunta){
@@ -20,11 +58,7 @@ class Multiplechoice{
     }
 
     public function obtenerpreguntas($numero){
-        return $this->preguntasExamen[$numero];
-    }
-
-    public function opciones($pregunta){
-        
+        return $this->pregun;
     }
 
 
