2882aecebdab240555d55342d2fff4f0196ac831
Nahuel Blando <nahuel.blando@gmail.com>
2018-11-30 00:22:21 -0300

    Agregado de testobtenerpreg

--- tests/MultiplechoiceTest.php
+++ tests/MultiplechoiceTest.php
@@ -4,6 +4,7 @@ namespace MultipleChoice;
 use PHPUnit\Framework\TestCase;
 
 class MultiplechoiceTest extends TestCase {
+    
     public function testcantdepreguntas(){
         $prueba = new Multiplechoice(12,1);
         $this->assertEquals($prueba->obtenercantpreguntas(),12);
--- tests/PreguntasTest.php
+++ tests/PreguntasTest.php
@@ -5,10 +5,21 @@ use Symfony\Component\Yaml\Yaml;
 use PHPUnit\Framework\TestCase;
 
 class PreguntasTest extends TestCase {
+
     public function testdescripcion(){
-		$yaml = Yaml::parseFile('tests/yamltest.yml');
-		$yaml = $yaml['preguntas'];
-		$pregunta = new pregunta($yaml[0]);
-		$this->assertEquals($pregunta->mostrarDesc(), "El término pixel hace referencia a");
-    }
+			$yaml = Yaml::parseFile('tests/yamltest.yml');
+			$yaml = $yaml['preguntas'];
+			$pregunta = new pregunta($yaml[0]);
+			$this->assertEquals($pregunta->mostrarDesc(), "El término pixel hace referencia a");
+		}
+		
+		public function testobtenerpreg () {
+			$yaml = Yaml::parseFile( 'tests/yamltest.yml' );
+			$yaml = $yaml['preguntas'];
+			$pregunta = new pregunta( $yaml[0] );
+			$this->assertTrue(in_array( "La unidad mínima de información de una imagen.", $pregunta->optenerpreg() ) );
+			$this->assertTrue(in_array( "La longitud de la diagonal de una imagen en pulgadas.", $pregunta->optenerpreg() ) );
+			$this->assertTrue(in_array( "La cantidad de puntos por pulgada.", $pregunta->optenerpreg() ) );
+			$this->assertTrue(in_array( "La cantidad de colores de un punto.", $pregunta->optenerpreg() ) );
+		}
 }
